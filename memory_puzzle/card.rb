325a308b3567d0eac199f0349b36aa4103d72718
App Academy Student <email@email.com>
2022-02-15 11:49:54 -0500

    commit card class

---
+++ memory_puzzle/card.rb
@@ -0,0 +1,26 @@
+class Card
+
+  def initialize(face_value)
+    @face_value = face_value
+    @face_up = true
+  end
+
+  def to_s
+    if @face_up 
+      @face_value 
+    end
+  end
+  
+  def hide
+    @face_up = false
+  end
+
+  def reveal
+    @face_value
+  end
+
+  def ==(card)
+    self.to_s == card.to_s
+  end
+
+end
