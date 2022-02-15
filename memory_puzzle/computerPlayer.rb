31dcbc1dbbca59dbf0bff14f3de57273df01b2f7
App Academy Student <email@email.com>
2022-02-15 17:30:41 -0500

    updates

--- memory_puzzle/computerPlayer.rb
+++ memory_puzzle/computerPlayer.rb
@@ -1,7 +1,3 @@
-require_relative 'card.rb'
-require_relative 'board.rb'
-require_relative 'game.rb'
-
 class ComputerPlayer
 
   def initialize
@@ -25,22 +21,32 @@ class ComputerPlayer
 
   def get_pos
     return_position = @available_positions.sample
-    if first_guess
-      known_positions.each do |face_value, positions|
-        return_position = positions[0] if positions.length == 2
+
+    if @first_guess
+      @known_positions.each do |face_value, positions|
+        return_position = positions if positions.length == 2
       end
     else
-      if @known_positions.has_key?[@revealed_card.to_s]
+      if @known_positions.has_key?(@revealed_card.to_s)
         return_position = @known_positions[@revealed_card.to_s]
       end
+    end
+
+    if !@known_positions.has_value?(return_position)
+      @available_positions.delete(return_position)
     end
     return_position
   end
 
-  def receive_revealed_card(pos, card)
+  def receive_revealed_card(pos, card, guess)
     @known_positions[card.to_s] << pos
-    @revealed_card = card
-    first_guess = !first_guess
+    @first_guess = guess
+    if @first_guess
+      @revealed_card = nil
+    else
+      @revealed_card = card 
+    end 
   end
+  
 
 end
--- memory_puzzle/game.rb
+++ memory_puzzle/game.rb
@@ -1,6 +1,7 @@
 require_relative "board.rb"
 require_relative "card.rb"
 require_relative "humanPlayer.rb"
+require_relative "computerPlayer.rb"
 
 class Game
   def initialize(size=4, player)
@@ -12,18 +13,18 @@ class Game
   end
 
   def valid?(guess)
-    if guess.length != 2
-      return false
-    end
-    if guess[0].to_i.to_s != guess[0] ||  guess[1].to_i.to_s != guess[1] 
-      return false
-    end
-    if (guess[0].to_i < 0 || guess[0].to_i >= @board.size) || (guess[1].to_i < 0 || guess[1].to_i >= @board.size)
-      return false
-    end
-    if @board[[guess[0].to_i,guess[1].to_i]].face_up 
-      return false
-    end
+    # if guess.length != 2
+    #   return false
+    # end
+    # if guess[0].to_i.to_s != guess[0] ||  guess[1].to_i.to_s != guess[1] 
+    #   return false
+    # end
+    # if (guess[0].to_i < 0 || guess[0].to_i >= @board.size) || (guess[1].to_i < 0 || guess[1].to_i >= @board.size)
+    #   return false
+    # end
+    # if @board[[guess[0].to_i,guess[1].to_i]].face_up 
+    #   return false
+    # end
     return true
   end
 
@@ -37,6 +38,7 @@ class Game
         p "Invalid guess. Guess in the form of: 0 1"
         pos = @player.get_pos
       end
+      #make_guess([pos[0].to_i, pos[1].to_i])
       make_guess([pos[0].to_i, pos[1].to_i])
     end
     system("clear")
@@ -48,7 +50,7 @@ class Game
     if @previous_guess.nil?
       @previous_guess = @board[pos]
       @board.reveal(pos)
-      @player.receive_revealed_card(pos, @board[pos])
+      @player.receive_revealed_card(pos, @board[pos], false)
     else
       if !(@previous_guess == @board.reveal(pos))
         system('clear')
@@ -56,6 +58,7 @@ class Game
         p "Try Again."
         sleep(1.5)
         system('clear')
+        @player.receive_revealed_card(pos, @board[pos], true)
         @previous_guess.hide
         @board[pos].hide
       end
@@ -63,5 +66,6 @@ class Game
     end
   end
 end
+
 game = Game.new(ComputerPlayer.new)
 game.play()
